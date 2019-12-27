9be0a9af7ae91e20cdbc6daeb223d30de6836551
Joe Manthey <jdmanthey@gmail.com>
2019-12-27 13:40:40 -0600

    Update 03_create_genotype_scripts.r

--- 02_trim_process_genotype/03_create_genotype_scripts.r
+++ 02_trim_process_genotype/03_create_genotype_scripts.r
@@ -29,19 +29,44 @@
 	
 	
 	# write the helper files for the 1st genotyping step
-	for(a in 1:nrow(individuals)) {
-		if(a == 1) {
-			helper1 <- cbind(faidx[,1], rep(a, nrow(faidx)))
+	helper <- c()
+	for(a in 1:nrow(faidx)) {
+		a_rep <- faidx[a,]
+		job_number <- ceiling(a_rep[1,2] / max_genotype_job_size)
+		job_number <- job_suffixes[1:job_number]
+		
+		if(length(job_number) > 1) {
+			# define interval to group genotype if more than one job
+			interval_start <- 1
+			interval_end <- max_genotype_job_size
+			for(b in 1:length(job_number)) {
+				helper <- rbind(helper, c(paste(faidx[a,1], ":", interval_start, "-", interval_end, sep="")))
+					interval_start <- interval_start + max_genotype_job_size
+				if((b+1) != length(job_number)) {
+					interval_end <- interval_end + max_genotype_job_size
+				} else {
+					interval_end <- faidx[a,2]
+				}
+			}
 		} else {
-			helper1 <- rbind(helper1, cbind(faidx[,1], rep(a, nrow(faidx))))
+			helper <- rbind(helper, paste(faidx[a,1], ":", 1, "-", faidx[a,2], sep=""))
 		}
 	}
+	for(a in 1:nrow(individuals)) {
+		if(a == 1) { 
+			helper1 <- cbind(helper, rep(a, nrow(helper)))
+		} else {
+			helper1 <- rbind(helper1, cbind(helper, rep(a, nrow(helper))))
+		}
+	}
+	
 	# write the chromosome/scaffold to genotype for each job
 	write(helper1[,1], file=paste(directory_name, "/01_gatk_split", "/helper1.txt", sep=""), ncolumns=1)
 	# write the individual to genotype for each job
 	write(helper1[,2], file=paste(directory_name, "/01_gatk_split", "/helper2.txt", sep=""), ncolumns=1)
 	
 	
+
 	
 	# step 1
 	# genotype all individuals using GATK, one array job using the above two helper files
@@ -53,7 +78,7 @@
 	write("#$ -S /bin/bash", file=a.script, append=T)
 	write(paste("#$ -N ", "step1", sep=""), file=a.script, append=T)
 	write(paste("#$ -q ", queue, sep=""), file=a.script, append=T)
-	write("#$ -pe sm 8", file=a.script, append=T)
+	write("#$ -pe sm 2", file=a.script, append=T)
 	write(paste("#$ -P ", cluster, sep=""), file=a.script, append=T)
 	write("#$ -l h_rt=48:00:00", file=a.script, append=T)
 	write("#$ -l h_vmem=8G", file=a.script, append=T)
@@ -68,7 +93,7 @@
 	
 	#gatk 4.0
 	a_name <- paste(project_directory, "/01_bam_files/", "${ind_array}", "_final.bam", sep="")
-	gatk_command <- paste('/lustre/work/jmanthey/gatk-4.1.0.0/gatk --java-options "-Xmx64g" HaplotypeCaller -R ', reference_genome_location, " -I ", a_name, " -ERC GVCF -O ", project_directory, "/02_vcf/", "${chr_array}", "._${ind_array}_.g.vcf", " --QUIET --intervals ", "${chr_array}", sep="")
+	gatk_command <- paste('/lustre/work/jmanthey/gatk-4.1.0.0/gatk --java-options "-Xmx16g" HaplotypeCaller -R ', reference_genome_location, " -I ", a_name, " -ERC GVCF -O ", project_directory, "/02_vcf/", "${chr_array}", "._${ind_array}_.g.vcf", " --QUIET --intervals ", "${chr_array}", sep="")
 	write(gatk_command, file=a.script, append=T)
 	
 	
@@ -76,12 +101,12 @@
 	
 	# write the helper files for the 2nd genotyping step
 	# write the chromosome/scaffold to database for each job
-	write(faidx[,1], file=paste(directory_name, "/02b_gatk_database", "/helper3.txt", sep=""), ncolumns=1)
+	write(unique(helper1[,1]), file=paste(directory_name, "/02b_gatk_database", "/helper3.txt", sep=""), ncolumns=1)
 
 	
 	
 	# step 2
-	# create genotyping database for each of the chromosomes
+	# create genotyping database for each of the chromosomes segments
 	a.script <- paste(directory_name, "/02b_gatk_database/step2_array.sh", sep="")
 	write("#!/bin/sh", file=a.script)
 	write("#$ -V", file=a.script, append=T)
@@ -89,11 +114,11 @@
 	write("#$ -S /bin/bash", file=a.script, append=T)
 	write(paste("#$ -N ", "step2", sep=""), file=a.script, append=T)
 	write(paste("#$ -q ", queue, sep=""), file=a.script, append=T)
-	write("#$ -pe sm 6", file=a.script, append=T)
+	write("#$ -pe sm 2", file=a.script, append=T)
 	write(paste("#$ -P ", cluster, sep=""), file=a.script, append=T)
 	write("#$ -l h_rt=48:00:00", file=a.script, append=T)
 	write("#$ -l h_vmem=10G", file=a.script, append=T)
-	write(paste("#$ -t 1:", nrow(faidx), sep=""), file=a.script, append=T)
+	write(paste("#$ -t 1:", length(unique(helper1[,1])), sep=""), file=a.script, append=T)
 	write("", file=a.script, append=T)
 	write("module load intel java", file=a.script, append=T)
 	write("", file=a.script, append=T)
@@ -110,7 +135,7 @@
 	}
 	
 	#gatk 4.0
-	gatk_command <- paste('/lustre/work/jmanthey/gatk-4.1.0.0/gatk --java-options "-Xmx60g" GenomicsDBImport ', vcf_total, " --genomicsdb-workspace-path ", project_directory, "/02_vcf/", "${chr_array}", " -L ", "${chr_array}", sep="")
+	gatk_command <- paste('/lustre/work/jmanthey/gatk-4.1.0.0/gatk --java-options "-Xmx20g" GenomicsDBImport ', vcf_total, " --genomicsdb-workspace-path ", project_directory, "/02_vcf/", "${chr_array}", " -L ", "${chr_array}", sep="")
 				
 	write(gatk_command, file=a.script, append=T)
 		
@@ -171,7 +196,7 @@
 	write("#$ -S /bin/bash", file=a.script, append=T)
 	write(paste("#$ -N ", "step3", sep=""), file=a.script, append=T)
 	write(paste("#$ -q ", queue, sep=""), file=a.script, append=T)
-	write("#$ -pe sm 10", file=a.script, append=T)
+	write("#$ -pe sm 14", file=a.script, append=T)
 	write(paste("#$ -P ", cluster, sep=""), file=a.script, append=T)
 	write("#$ -l h_rt=48:00:00", file=a.script, append=T)
 	write("#$ -l h_vmem=8G", file=a.script, append=T)
@@ -187,7 +212,7 @@
 	write("", file=a.script, append=T)
 
 	#gatk 4.0
-	gatk_command <- paste('/lustre/work/jmanthey/gatk-4.1.0.0/gatk --java-options "-Xmx80g" GenotypeGVCFs -R ', reference_genome_location, " -V gendb://", project_directory, "/02_vcf/", "${chr_array}", " --include-non-variant-sites -O ", project_directory, "/03_vcf/", "${name_array}", ".g.vcf", " -L ", "${interval_array}", sep="")
+	gatk_command <- paste('/lustre/work/jmanthey/gatk-4.1.0.0/gatk --java-options "-Xmx112g" GenotypeGVCFs -R ', reference_genome_location, " -V gendb://", project_directory, "/02_vcf/", "${interval_array}", " --include-non-variant-sites -O ", project_directory, "/03_vcf/", "${name_array}", ".g.vcf", " -L ", "${interval_array}", sep="")
 	write(gatk_command, file=a.script, append=T)
 		
 		
