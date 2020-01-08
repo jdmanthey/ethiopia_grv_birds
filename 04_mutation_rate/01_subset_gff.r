5ba784f1a097dc3a13a93a6e7cd0c367d0eebb2d
Joe Manthey <jdmanthey@gmail.com>
2020-01-08 13:32:53 -0600

    Update 01_subset_gff.r

--- 04_mutation_rate/01_subset_gff.r
+++ 04_mutation_rate/01_subset_gff.r
@@ -1,13 +1,16 @@
 # make a gff file for each of the cds features in the overall gff file
 
-x <- read.table("GCF_000151805.1_Taeniopygia_guttata-3.2.4_genomic.gff", stringsAsFactors=F, sep="", fill=T)
+x <- read.table("GCF_000151805.1_Taeniopygia_guttata-3.2.4_genomic.gff", stringsAsFactors=F, sep="\t", fill=T)
 
 # keep only cds
-x <- x[x[,3] == "CDS",]
+x2 <- x[x[,3] == "CDS",]
 
-# identify the unique genes
-gene_list <- sapply(strsplit(sapply(strsplit(x[,9], ";"), "[[", 1), "="), "[[", 2)
-unique_gene_list <- unique(gene_list)
+# change row names
+rownames(x2) <- seq(from=1, to=nrow(x2), by=1)
+
+# keep only basic attribute data
+x2[,9] <- sapply(strsplit(x2[,9], ";"), "[[", 1)
+
 
 # write the cds to file
-write.table(x, file="finch_cds.gff", sep="\t", row.names=F, col.names=F, quote=F)
+write.table(x2, file="finch_cds2.gff", sep="\t", row.names=F, col.names=F, quote=F)
