0d6eaeca23c966b610c3a570074b7f2acff00be9
Joe Manthey <jdmanthey@gmail.com>
2021-09-29 09:26:37 -0500

    Update 05_sum_trees.sh

--- 08_revisions/05_sum_trees.sh
+++ 08_revisions/05_sum_trees.sh
@@ -1,3 +1,6 @@
 # maximum clade credibility tree (simple summarization) from all gene trees using dendropy:
 # gives info about which nodes have support from what proportion of gene trees
 sumtrees.py --output=ethiopia_100kbp.tre --min-clade-freq=0.05 ethiopia.trees 
+
+#astral
+java -jar ~/Astral/astral.5.6.3.jar -i ethiopia.trees -o ethiopia_100kbp_astral.tre 2> ethiopia_100kbp_astral.log
