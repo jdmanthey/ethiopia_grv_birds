0ecedc86c241cfcd24295794d3047b944fc32980
Joe Manthey <jdmanthey@gmail.com>
2021-10-11 09:43:12 -0500

    Create 04_parophasma_multihetsep.sh

---
+++ 08_revisions/06_msmc-im/04_parophasma_multihetsep.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=msmc_par
+#SBATCH --partition quanah
+#SBATCH --nodes=1 --ntasks=2
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-38
+
+# define individual array for input names (can be numbers of characters, depending on naming scheme)
+ind_array=(12 13 14)
+
+# read in scaffolds to use
+chr=$( head -n${SLURM_ARRAY_TASK_ID} scaffold_list.txt | tail -n1 )
+
+# prefix of output
+prefix="parophasma"
+
+# define working directory
+work_dir=/lustre/scratch/jmanthey/03_ethiopia_popgen/04_msmc/
+
+# define output directory (make sure it is already made)
+out_dir=/lustre/scratch/jmanthey/03_ethiopia_popgen/05_msmc_input/
+
+# list of vcf files for each individual
+vcf_list=( "${ind_array[@]/%/.${chr}.whatshap.vcf.gz}" )
+vcf_list=( "${vcf_list[@]/#/${work_dir}}" )
+vcf_list=$(printf " %s" "${vcf_list[@]}")
+echo $vcf_list
+
+# list of mask files for each individual
+mask_list=( "${ind_array[@]/%/.${chr}.samtools.bed.gz}" )
+mask_list=( "${mask_list[@]/#/--mask ${work_dir}ind_mask.}" )
+mask_list=$(printf " %s" "${mask_list[@]}")
+echo $mask_list
+
+/home/jmanthey/msmc-tools-master/generate_multihetsep.py $mask_list $vcf_list > ${out_dir}${prefix}.${chr}.multihetsep.txt
