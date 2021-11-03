275b7d12ea1ee55c8d5133c741e5a140384b1cb7
Joe Manthey <jdmanthey@gmail.com>
2021-11-03 11:32:29 -0500

    Update 04_melaenornis_multihetsep.sh

--- 08_revisions/06_msmc-im/04_melaenornis_multihetsep.sh
+++ 08_revisions/06_msmc-im/04_melaenornis_multihetsep.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --array=1-38
 
-# define individual array for input names (can be numbers of characters, depending on naming scheme)
-ind_array=(6 7 11 8 9 10)
+# define individual array for input names (can be numbers or characters, depending on naming scheme)
+ind_array=(6 7 8 9)
 
 # read in scaffolds to use
 chr=$( head -n${SLURM_ARRAY_TASK_ID} scaffold_list.txt | tail -n1 )
@@ -20,7 +20,7 @@ prefix="melaenornis"
 work_dir=/lustre/scratch/jmanthey/03_ethiopia_popgen/04_msmc/
 
 # define output directory (make sure it is already made)
-out_dir=/lustre/scratch/jmanthey/03_ethiopia_popgen/05_msmc_input/
+out_dir=/lustre/scratch/jmanthey/03_ethiopia_popgen/07_msmc_input/
 
 # list of vcf files for each individual
 vcf_list=( "${ind_array[@]/%/.${chr}.whatshap.vcf.gz}" )
@@ -29,9 +29,10 @@ vcf_list=$(printf " %s" "${vcf_list[@]}")
 echo $vcf_list
 
 # list of mask files for each individual
-mask_list=( "${ind_array[@]/%/.${chr}.samtools.bed.gz}" )
-mask_list=( "${mask_list[@]/#/--mask ${work_dir}ind_mask.}" )
+mask_list=( "${ind_array[@]/%/.${chr}.coverage.mask.gz}" )
+mask_list=( "${mask_list[@]/#/--mask ${work_dir}}" )
 mask_list=$(printf " %s" "${mask_list[@]}")
 echo $mask_list
 
 /home/jmanthey/msmc-tools-master/generate_multihetsep.py $mask_list $vcf_list > ${out_dir}${prefix}.${chr}.multihetsep.txt
+
